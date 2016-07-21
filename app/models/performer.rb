1f975f432684afc5166c5161a1177f043bd46205
Casey Ann Cumbow <cumbow8@gmail.com>
2016-07-21 14:05:51 -0600

    adds the rest of the relationships

--- app/models/performer.rb
+++ app/models/performer.rb
@@ -2,4 +2,6 @@ class Performer < ActiveRecord::Base
   has_many :performer_films
   has_many :films, through: :performer_films
   has_many :images, as: :imageable
+  has_many :reviews, as: :reviewable
+  has_many :votes, as: :votable
 end
