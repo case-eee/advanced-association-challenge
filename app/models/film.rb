d5700164fb975c20d7a1228663e340dcad45e800
Casey Ann Cumbow <cumbow8@gmail.com>
2016-07-21 14:03:45 -0600

    adds film associations

--- app/models/film.rb
+++ app/models/film.rb
@@ -2,4 +2,7 @@ class Film < ActiveRecord::Base
   belongs_to :category
   has_many :performer_films
   has_many :performers, through: :performer_films
+  has_many :reviews, as: :reviewable
+  has_many :images, as: :imageable
+  has_many :votes, as: :votable
 end
